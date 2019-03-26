4a18a1dbd97e82e9426cc2e4ba27b62bdd112212
Uncho <sergeychang@gmail.com>
2019-03-27 01:26:05 +0800

    user's `main()`

--- src/src/main.rs
+++ src/src/main.rs
@@ -1,3 +1,16 @@
-fn main(){
+#![no_std]
+#![no_main]
 
+fn kernel_entry() -> ! {
+    loop {
+        let c = '1';
+
+        match c {
+            '1' => {}
+            '2' => {}
+            _ => {}
+        }
+    }
 }
+
+raspi3_boot::entry!(kernel_entry);
